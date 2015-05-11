296895386f41a548a23ccbf49d0478f9d4d95f86
MartinBorsdorf <martin@mark3>
2015-05-12 00:00:22 +0200

    file.java mod.

--- Musixmeta/src/file.java
+++ Musixmeta/src/file.java
@@ -1,8 +1,20 @@
 
+import java.nio.file.Path;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Romeo Beckert, Martin Borsdorf
  */
+//@Path("file")
+
+@XmlRootElement(name = "file")
 public class file {
     /*
     name
